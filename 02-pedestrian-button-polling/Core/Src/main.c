9172f6c169425a819bd6595a34a211f05b6b1dda
sangwonlee06 <sangwonlee0622@outlook.com>
2026-06-28 18:50:29 -0700

    Copy project 01 behavior and fix startup bugs for project 02

--- 02-pedestrian-button-polling/Core/Src/main.c
+++ 02-pedestrian-button-polling/Core/Src/main.c
@@ -49,6 +49,14 @@ I2S_HandleTypeDef hi2s3;
 SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
+typedef enum
+{
+  TRAFFIC_RED,
+  TRAFFIC_YELLOW,
+  TRAFFIC_GREEN
+} TrafficState;
+static TrafficState traffic_state = TRAFFIC_RED;
+static uint32_t state_started_ms = 0;
 
 /* USER CODE END PV */
 
@@ -109,6 +117,11 @@ int main(void)
   MX_SPI1_Init();
   MX_USB_HOST_Init();
   /* USER CODE BEGIN 2 */
+  state_started_ms = HAL_GetTick();
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
 
   /* USER CODE END 2 */
 
@@ -120,6 +133,35 @@ int main(void)
     MX_USB_HOST_Process();
 
     /* USER CODE BEGIN 3 */
+    uint32_t now = HAL_GetTick();
+    if (traffic_state == TRAFFIC_RED) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+
+      if ((now - state_started_ms) >= 3000U) {
+        traffic_state = TRAFFIC_YELLOW;
+        state_started_ms = now;
+      }
+    } else if (traffic_state == TRAFFIC_YELLOW) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+
+      if ((now - state_started_ms) >= 1000U) {
+        traffic_state = TRAFFIC_GREEN;
+        state_started_ms = now;
+      }
+    } else if (traffic_state == TRAFFIC_GREEN) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+
+      if ((now - state_started_ms) >= 3000U) {
+        traffic_state = TRAFFIC_RED;
+        state_started_ms = now;
+      }
+    }
   }
   /* USER CODE END 3 */
 }
@@ -355,6 +397,9 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(OTG_FS_PowerSwitchOn_GPIO_Port, OTG_FS_PowerSwitchOn_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin
                           |Audio_RST_Pin, GPIO_PIN_RESET);
 
@@ -389,6 +434,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA1 PA2 PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LD4_Pin LD3_Pin LD5_Pin LD6_Pin
                            Audio_RST_Pin */
